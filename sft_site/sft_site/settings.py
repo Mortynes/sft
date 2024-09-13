1270a04ce82a36362d7fdc312bf9d81ed4d0b764
Marat <clever1mistory@gmail.com>
2024-09-13 13:15:45 +0300

    whitenoiseh

--- sft_site/sft_site/settings.py
+++ sft_site/sft_site/settings.py
@@ -119,7 +119,7 @@ if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
